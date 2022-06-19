c641335f37acdddd39f76df8ec42e0b97f5458ee
Ekaterina986 <kev986@mail.ru>
2022-06-19 15:31:25 +0300

    fest commit

---
+++ Задача 7.4.py
@@ -0,0 +1,216 @@
+list_students = []
+list_lecturers = []
+
+class Student:
+	
+	def __init__ (self, name, surname,gender):
+		self.name = name
+		self.surname = surname
+		self.gender = gender
+		self.finished_courses = []
+		self.courses_in_progress = []
+		self.grades = {}
+		list_students.append(self)
+
+# оценка лектора	
+
+	def rate_lecturer(self, lecturer, course, grade):
+		if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+			if course in lecturer.grades_lecturer:
+	 			lecturer.grades_lecturer[course] += [grade]
+			else:
+				lecturer.grades_lecturer[course] = [grade]
+		else:
+			return 'Ошибка'
+
+# расчет средней оценки
+	def count_average(self):
+		summa = 0
+		count = 0
+		for grades in self.grades.values():
+			for grade in grades:
+				summa += grade
+				count += 1
+		if count == 0:
+			return 'Пока нет оценок'
+		return summa / count
+
+# сравнить студентов по средней оценке
+
+	def __lt__(self, foe):
+		if not isinstance(foe, Student):
+			print('Нет такого студента')
+			return
+		return self.count_average() < foe.count_average()
+
+
+
+# метод str
+
+	def __str__(self):
+		finish_str = ', '.join(self.finished_courses)
+		progress_str = ', '.join(self.courses_in_progress)
+		res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.count_average()}\nКурсы в процессе изучения: {progress_str}\nЗавершенные курсы: {finish_str}'
+		return res
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+class Lecturer(Mentor):
+	
+	def __init__(self,name, surname):
+		super().__init__(name, surname)
+		self.grades_lecturer = {}
+		list_lecturers.append(self)
+
+# расчет средней оценки
+
+	def count_average(self):
+		summa = 0
+		count = 0
+
+		for grades in self.grades_lecturer.values():
+			for grade in grades:
+				summa += grade
+				count += 1
+		if count == 0:
+			return 'Пока нет оценок'
+		return summa / count
+
+# сравнить лекторов по средней оценке
+
+	def __lt__(self, foe):
+		if not isinstance(foe, Lecturer):
+			print('Нет такого лектора')
+			return
+		return self.count_average() < foe.count_average()
+
+# метод str 
+	def __str__(self):
+		res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.count_average()}'
+		return res
+
+
+
+class Reviewer(Mentor):
+
+# оценка студента
+
+	def rate_student(self, student, course, grade):
+		if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+			if course in student.grades:
+				student.grades[course] += [grade]
+			else:
+				student.grades[course] = [grade]
+		else:
+			return 'Ошибка'
+
+# метод str 
+
+	def __str__(self):
+		res = f'Имя: {self.name}\nФамилия: {self.surname}'
+		return res
+
+
+# средняя оценка за курс
+
+# студенты
+
+def grade_average(list_students, course):
+	summa = 0
+	count = 0
+	for student in list_students:
+		if course in student.courses_in_progress:
+			res = sum(student.grades[course]) / len(student.grades[course])
+			summa += res
+			count += 1
+			return
+	if count == 0:
+		return'Нет такого курса'
+	return f'Средний бал по курсу: "{course}": {summa / count}'
+
+# лекторы
+
+def grade_average_lect(list_lecturers, course):
+	summa = 0
+	count = 0
+	for lecturer in list_lecturers:
+		if course in lecturer.courses_attached:
+			res = sum(lecturer.grades_lecturer[course]) / len(lecturer.grades_lecturer[course])
+			summa += res
+			count += 1
+	if count == 0:
+		return'Нет такого курса'
+	return f'Средний бал за лекции по курсу: "{course}": {summa / count}'
+
+cool_lecturer = Lecturer('Sherlock', 'Holmes')
+cool_lecturer.courses_attached += ['Python']
+cool_lecturer.courses_attached += ['C++']
+
+best_lecturer = Lecturer('Hercule', 'Poirot')
+best_lecturer.courses_attached += ['JS']
+best_lecturer.courses_attached += ['C++']
+
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+cool_reviewer.courses_attached += ['C++']
+
+best_reviewer = Reviewer('Buddy', 'Some')
+best_reviewer.courses_attached += ['JS']
+best_reviewer.courses_attached += ['C++']
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['JS']
+best_student.finished_courses += ['C++']
+
+best_student.rate_lecturer(cool_lecturer, 'Python',10)
+best_student.rate_lecturer(cool_lecturer, 'JS',8)
+best_student.rate_lecturer(cool_lecturer, 'C++',9)
+
+best_student.rate_lecturer(best_lecturer, 'Python',10)
+best_student.rate_lecturer(best_lecturer, 'JS',9)
+best_student.rate_lecturer(best_lecturer, 'C++',10)
+
+worst_student = Student('Oleg', 'Oleg', 'your_gender')
+worst_student.courses_in_progress += ['Python']
+worst_student.courses_in_progress += ['Java']
+worst_student.finished_courses += ['C++']
+
+worst_student.rate_lecturer(cool_lecturer, 'Python',6)
+worst_student.rate_lecturer(cool_lecturer, 'Python',6)
+worst_student.rate_lecturer(cool_lecturer, 'C++',10)
+
+cool_reviewer.rate_student(worst_student, 'Python', 4)
+cool_reviewer.rate_student(worst_student, 'C++', 6)
+cool_reviewer.rate_student(worst_student, 'Python', 8)
+ 
+cool_reviewer.rate_student(best_student, 'Python', 10)
+cool_reviewer.rate_student(best_student, 'C++', 6)
+cool_reviewer.rate_student(best_student, 'Python', 8)
+
+best_reviewer.rate_student(best_student, 'Python', 10)
+best_reviewer.rate_student(best_student, 'C++', 6)
+best_reviewer.rate_student(best_student, 'Python', 8)
+
+
+print(best_student)
+print(worst_student)
+print(best_lecturer)
+print(cool_lecturer)
+print(cool_reviewer)
+print(best_reviewer)
+print(best_student.count_average())
+print(worst_student.count_average())
+print(best_student > worst_student)
+print(best_lecturer.count_average())
+print(cool_lecturer.count_average())
+print(cool_lecturer > best_lecturer)
+print(*list_students, sep = ', ')
+print(worst_student.grades)
+print(grade_average(list_students, 'Python'))
+print(grade_average_lect(list_lecturers, 'JS'))
+print(best_lecturer.grades_lecturer)
